3441173377817fead4e8711a3cbb9d4880681d8f
Kamenraide <kamenraiderpowerranger47@gmail.com>
2022-06-22 10:50:51 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>hello world!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda laboriosam voluptate aliquam ut optio, praesentium repellendus doloribus asperiores temporibus sapiente animi, dolores, id facilis culpa sint nostrum veritatis quam velit?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
